e7de7b05329218cc25cb8d58e4647b07b8b2987e
to-waiting-room <to.waiting.room@gmail.com>
2025-07-17 16:18:11 +0100

    Changes to be committed: 	modified:   __pycache__/tax_calculator.cpython-311.pyc 	modified:   __pycache__/test_tax_calculator.cpython-311-pytest-7.4.3.pyc 	modified:   tax_calculator.py 	modified:   test_tax_calculator.py

--- tax_calculator.py
+++ tax_calculator.py
@@ -34,65 +34,13 @@ def main():
     print(f"Non-ISA Saving Interest: £{non_isa_saving_interest}")
     print("------------------------------")
     print(f'Personal allowance: £{get_personal_allowance(salary)}')
-    print(f'Income tax: £{calculate_income_tax(salary)}')
-    print(f'Taxable saving interest: {taxable_saving(salary, non_isa_saving_interest)}')
+    print(f'Total income tax: £{calculate_income_tax(salary, non_isa_saving_interest)}')
+    print(f'Income tax from salary: £{calculate_income_tax(salary)}')
+    print(f'Tax from saving interest: £{tax_from_saving_interest(salary, non_isa_saving_interest)}')
+    print(f'Taxable saving interest: {taxable_saving_interest(salary, non_isa_saving_interest)}')
+    print("------------------------------")
+    print(f'testing resulst: {testing(salary, non_isa_saving_interest)}')
     
-
-
-
-def get_saving_allowance(income): 
-    pass
-
-    annual_income = int(income) if isinstance(income, (int, float)) else float(income)
-
-    # calculate if there's any personal allowance base on income
-    # if income is below 12570, personal allowance is 5000
-    # if income is above 12570, personal allowance is any amount above 12570 subtracted from 5000
-    # if income is above 17570, personal allowance is 0
-    start_rate_for_saving_limit = pa_start_rate() # 5000
-    pa_upper_limit = pa_upper_limit() # 17570
-    pa_lower_limit = pa_upper_limit - start_rate_for_saving_limit
-
-    # logic to determine how much is deducted from the £5000 personal allowance based on income
-    if annual_income > pa_upper_limit:
-        start_rate_of_saving = 0
-    elif annual_income > pa_lower_limit:
-        start_rate_of_saving = start_rate_for_saving_limit - (annual_income - pa_lower_limit)
-    else: 
-        start_rate_of_saving = start_rate_for_saving_limit
-
-    # set personal allowance as the same as basic rate
-    # as anyone with income below 12570, for any amount that they exceed 12570 + 5000, will still have the personal allowance of 1000 under basic rate
-    personal_saving_allowance = get_psa()
-
-    # tax brackets base on annnual income
-    tax_brackets = get_tax_brackets()
-
-    if annual_income == 0: 
-        current_tax_bracket = 'personal_allowance'
-    else: 
-        current_tax_bracket = tax_brackets[(annual_income > tax_brackets['lower_limit']) & (annual_income <= tax_brackets['upper_limit'])]
-
-        if current_tax_bracket.empty:
-            raise ValueError("No applicable tax bracket found for the given income.")
-            
-        current_tax_bracket = current_tax_bracket['bracket'].values[0]
-
-    # now based on the current tax bracket, calculate how much interest you can earn without paying tax
-    # if current_tax_bracket == 'personal_allowance':
-    #     tax_free_interest = personal_saving_allowance['personal_allowance']
-    # else: 
-    tax_free_interest = personal_saving_allowance[current_tax_bracket] + start_rate_of_saving
-
-
-
-    return tax_free_interest
-
-    # if you donn't have any personal income, you have a personal saving allowance of 5000
-    # on top of it, you still have the personal allownace of 12570 with tax rate of 0%
-    # so if you earn interest on saving up to 17570 without any other income, you won't pay any tax on it
-    # if you earn interest on saving above 17570, you will pay tax on any amount that exceeds 17570 based on the tax breackets
-
 def get_personal_allowance(non_savings_income): 
     pa_start_rate_limit = pa_start_rate()
     pa_upper_limit_rate = pa_upper_limit()
@@ -102,11 +50,14 @@ def get_personal_allowance(non_savings_income):
         pa = min(pa_upper_limit_rate - non_savings_income, pa_start_rate_limit)
     return pa
     
-def taxable_saving(non_saving_income, non_isa_saving_interest=0): 
+def taxable_saving_interest(non_saving_income, non_isa_saving_interest=0): 
     '''
     get the taxable saving interest based on non_saving_income and non_isa_saving_interest
     '''
     non_saving_income = float(non_saving_income)
+    non_isa_saving_interest = float(non_isa_saving_interest)
+
+    total_non_isa_income = non_saving_income + non_isa_saving_interest
 
     # get parameters
     tax_brackets = get_tax_brackets()
@@ -114,13 +65,13 @@ def taxable_saving(non_saving_income, non_isa_saving_interest=0):
     psa_brackets = get_psa()
     
     # find out which tax bracket the non_saving_income is in
-    if non_saving_income <= 0: 
+    if total_non_isa_income <= 0: 
         tax_bracket = 'personal_allowance'
 
     for bracket in tax_brackets:
         lower = bracket['lower_limit']
         upper = bracket['upper_limit']
-        if lower < non_saving_income <= upper:
+        if lower < total_non_isa_income <= upper:
             tax_bracket =  bracket['bracket']
     
     # calculate deducatable from pa and psa
@@ -142,35 +93,19 @@ def taxable_saving(non_saving_income, non_isa_saving_interest=0):
 
 
 def calculate_income_tax(non_saving_income, non_isa_saving_interest=0):
-    # brackets = get_tax_brackets()
-    # tax_due = 0.0
+    non_saving_income = float(non_saving_income)
+    non_isa_saving_interest = float(non_isa_saving_interest)
 
-    # for _, row in brackets.iterrows():
-    #     lower = row['lower_limit']
-    #     upper = row['upper_limit']
-    #     rate = row['rate']
-        
-    #     # If income is less than the lower limit, skip this bracket
-    #     if income <= lower:
-    #         continue
-        
-    #     # Taxable amount in this bracket
-    #     taxable = min(income, upper) - lower
-    #     bracket_tax = taxable * rate
+    taxable_interest = taxable_saving_interest(non_saving_income, non_isa_saving_interest)['taxable_saving_interest']
 
-    #     if bracket_tax < 0: 
-    #         raise ValueError(f"The {row['bracket']} bracket is returning negative tax of {bracket_tax}. ")
+    taxable_income = non_saving_income + taxable_interest
 
-    #     tax_due += bracket_tax
 
-    # return tax_due
+
     # get parameters
     tax_bracket = get_tax_brackets()
-    pa_start_rate_limit = pa_start_rate()
-    pa_upper_limit_rate = pa_upper_limit()
-    psa_brackets = get_psa()
-
-    # calcaulate tax from non-savings income
+    
+    # calcaulate tax from taxable income
     tax_due = 0.0
 
     for _, row in tax_bracket.iterrows():
@@ -179,24 +114,22 @@ def calculate_income_tax(non_saving_income, non_isa_saving_interest=0):
         rate = row['rate']
         
         # If income is less than the lower limit, skip this bracket
-        if non_saving_income <= lower:
+        if taxable_income <= lower:
             bracket_tax = 0.0
     
         # Taxable amount in this bracket
         else: 
-            taxable = min(non_saving_income, upper) - lower
+            taxable = min(taxable_income, upper) - lower
             bracket_tax = taxable * rate
 
         if bracket_tax < 0: 
             raise ValueError(f"The {row['bracket']} bracket is returning negative tax of {bracket_tax}. ")
         
         tax_due += bracket_tax
-
-    # calculate if the saving interest is taxable
-        
+    
     return tax_due
 
-def tax_from_saving_interest(salary, interest_earned, isa_interest=0):
+def tax_from_saving_interest(salary, non_isa_saving_interest):
     pass
     """
     Calculate the tax owed from interest earned on savings.
@@ -204,29 +137,40 @@ def tax_from_saving_interest(salary, interest_earned, isa_interest=0):
     Parameters:
     - income: Annual income of the individual.
     - interest_earned: Total interest earned from savings.
-    - tax_free_interest: Optional; the amount of interest that is tax-free.
     
     Returns:
     - Tax owed from the interest earned.
     """
-    if not all(isinstance(x, (int, float)) for x in [salary, interest_earned, isa_interest]): 
+    if not all(isinstance(x, (int, float)) for x in [salary, non_isa_saving_interest]): 
         raise TypeError("All inputs must be numeric (int or float).")
 
-    saving_allowance = get_saving_allowance(salary)
-
-    taxable_interest = interest_earned - isa_interest - saving_allowance
-    if taxable_interest < 0:
-        taxable_interest = 0
-
-    taxable_income = salary + taxable_interest
-    
     tax_from_salary = calculate_income_tax(salary)
-    tax_from_all_income = calculate_income_tax(taxable_income)
-    
-    tax_from_saving_interest = tax_from_all_income - tax_from_salary
-  
+
+    total_income_tax = calculate_income_tax(salary, non_isa_saving_interest)
+
+    tax_from_saving_interest = total_income_tax - tax_from_salary
+
     return tax_from_saving_interest
 
+
+def testing(non_saving_income, non_isa_saving_interest=0):
+
+    non_saving_income = float(non_saving_income)
+    non_isa_saving_interest = float(non_isa_saving_interest)
+
+    total_non_isa_income = non_saving_income + non_isa_saving_interest
+
+    deductable_interest = taxable_saving_interest(non_saving_income)['personal_allowance'] + taxable_saving_interest(non_saving_income)['psa']
+
+    taxable_income = total_non_isa_income - deductable_interest
+
+    return {
+        'non_saving_income': non_saving_income,
+        'non_isa_saving_interest': non_isa_saving_interest,
+        'total_non_isa_income': total_non_isa_income,
+        'deductable_interest': deductable_interest,
+        'taxable_income': taxable_income
+    }
 
 if __name__ == "__main__":
     main()
--- test_tax_calculator.py
+++ test_tax_calculator.py
@@ -1,18 +1,7 @@
 import pandas as pd
 import pytest
 # from pandas.testing import assert_frame_equal
-from tax_calculator import get_personal_allowance, calculate_income_tax, tax_from_saving_interest, non_saving_tax_bracket
-
-# test tax_bracket function
-def test_non_saving_tax_bracket(): 
-    assert non_saving_tax_bracket(0) == 'personal_allowance'
-    assert non_saving_tax_bracket(12570) == 'personal_allowance'
-    assert non_saving_tax_bracket(12570.01) == 'basic_rate'
-    assert non_saving_tax_bracket(50270) == 'basic_rate'
-    assert non_saving_tax_bracket(50270.01) == 'higher_rate'
-    assert non_saving_tax_bracket(125140) == 'higher_rate'
-    assert non_saving_tax_bracket(125140.01) == 'additional_rate'
-    assert non_saving_tax_bracket(200000) == 'additional_rate'
+from tax_calculator import get_personal_allowance, calculate_income_tax, tax_from_saving_interest
 
 # personal allowance tests
 def test_get_personal_allowance_full_5000(): 
@@ -59,67 +48,102 @@ def test_calculate_income_tax_additional_rate():
     assert calculate_income_tax(150000) == 48675 #0.45 * (150000 - 125140) + 0.40 * (125140 - 50270) + 0.20 * (50270 - 12570)
     assert calculate_income_tax(200000) == 71175 #0.45 * (200000 - 125140) + 0.40 * (125140 - 50270) + 0.20 * (50270 - 12570)
 
+# income tax tests with savings interest
+## to be action
+# saving interest tax tests
+def test_tax_from_saving_interest_0(): 
+    assert tax_from_saving_interest(0, 0) == 0
+    # any non-saving income with no saving income should return 0 tax from saving interest
+    assert tax_from_saving_interest(1000, 0) == 0
+    assert tax_from_saving_interest(12570, 0) == 0
+    assert tax_from_saving_interest(50270, 0) == 0
+    assert tax_from_saving_interest(125140, 0) == 0
+    # anyone does not exceed their pa and psa should return 0 tax from saving interest
+    assert tax_from_saving_interest(0, 17570) == 0
+    assert tax_from_saving_interest(1000, 17570) == 0
+    assert tax_from_saving_interest(12569, 5000) == 0 # 12570 or 12569? 
+    assert tax_from_saving_interest(12569, 6000) == 0 # 12570 or 12569? 
+    # to be continued with more scenarios  with income but no taxable saving interest
 
-# to be actioned
+# scenario testings taken from moneysavingexpert.com
+# https://www.moneysavingexpert.com/savings/tax-free-savings/
+def test_scenario_1(): 
+    '''
+    Chris: Earns £14,500 from work, has £2,500 of savings income
 
-# # income tax tests with savings interest
-# ## to be action
-# # saving interest tax tests
-# def test_tax_from_saving_interest_0(): 
-#     assert tax_from_saving_interest(0, 0) == 0
-#     # any non-saving income with no saving income should return 0 tax from saving interest
-#     assert tax_from_saving_interest(1000, 0) == 0
-#     assert tax_from_saving_interest(12570, 0) == 0
-#     assert tax_from_saving_interest(50270, 0) == 0
-#     assert tax_from_saving_interest(125140, 0) == 0
-#     # anyone does not exceed their pa and psa should return 0 tax from saving interest
-#     assert tax_from_saving_interest(0, 17570) == 0
-#     assert tax_from_saving_interest(1000, 17570) == 0
-#     assert tax_from_saving_interest(12569, 5000) == 0 # 12570 or 12569? 
-#     assert tax_from_saving_interest(12569, 6000) == 0 # 12570 or 12569? 
-#     # to be continued with more scenarios  with income but no taxable saving interest
+    Chris will pay £386 in tax, all on his income from work. This is because he earns £1,930 more than his personal tax allowance of £12,570. 
+    However, all his savings interest will be tax-free, as it's covered by the starting savings rate.
+    As Chris earns more than £12,570, his income from work starts to eat in to his £5,000 starting savings rate allowance. 
+    He loses £1 of starting savings allowance for every £1 he earns over his personal allowance, so he's left with £3,070 (£5,000 to £1,930) of his starting savings allowance.
+    As this £3,070 more than covers his £2,500 interest, he doesn't owe any tax on it.
+    '''
+    assert calculate_income_tax(14500, 2500) == 386
+    assert calculate_income_tax(14500) == 386
+    assert tax_from_saving_interest(14500, 2500) == 0
 
-# # scenario testings taken from moneysavingexpert.com
-# # https://www.moneysavingexpert.com/savings/tax-free-savings/
-# def test_scenario_1(): 
-#     '''
-#     Chris: Earns £14,500 from work, has £2,500 of savings income
+def test_tax_from_saving_interest_scenario_2(): 
+    '''
+    Cheryl: No income from work, has £20,000 of savings income
 
-#     Chris will pay £386 in tax, all on his income from work. This is because he earns £1,930 more than his personal tax allowance of £12,570. 
-#     However, all his savings interest will be tax-free, as it's covered by the starting savings rate.
-#     As Chris earns more than £12,570, his income from work starts to eat in to his £5,000 starting savings rate allowance. 
-#     He loses £1 of starting savings allowance for every £1 he earns over his personal allowance, so he's left with £3,070 (£5,000 to £1,930) of his starting savings allowance.
-#     As this £3,070 more than covers his £2,500 interest, he doesn't owe any tax on it.
-#     '''
-#     assert calculate_income_tax(14500, 2500) == 386
-#     assert tax_from_saving_interest(14500, 2500) == 0
+    In this scenario, Cheryl will need to pay tax of just £286. As she has no earned income, the savings interest is mostly covered by a combination of allowances:
+    - Personal allowance - the first £12,570 is tax-free
+    - Starting savings rate - the next £5,000 is tax-free, so now £17,570 of the interest income is taxed at 0%
+    - Personal savings allowance - means the next £1,000 is tax-free, so £18,570 is taxed at 0%.
+    This leaves Cheryl with £1,430 of savings income which she will need to pay tax on. As she has no other income, this will be charged at the basic 20% rate, so she'll pay £286 in tax. 
+    '''
+    assert calculate_income_tax(0, 20000) == 286
+    assert calculate_income_tax(0) == 0
+    assert tax_from_saving_interest(0, 20000) == 286
 
-# def test_tax_from_saving_interest_scenario_2(): 
-#     '''
-#     Cheryl: No income from work, has £20,000 of savings income
+def test_tax_from_saving_interest_scenario_3(): 
+    '''
+    Hannah: Earns £35,000 from work, has £5,000 of savings income
 
-#     In this scenario, Cheryl will need to pay tax of just £286. As she has no earned income, the savings interest is mostly covered by a combination of allowances:
-#     - Personal allowance - the first £12,570 is tax-free
-#     - Starting savings rate - the next £5,000 is tax-free, so now £17,570 of the interest income is taxed at 0%
-#     - Personal savings allowance - means the next £1,000 is tax-free, so £18,570 is taxed at 0%.
-#     This leaves Cheryl with £1,430 of savings income which she will need to pay tax on. As she has no other income, this will be charged at the basic 20% rate, so she'll pay £286 in tax. 
-#     '''
-#     assert calculate_income_tax(0, 20000) == 286
-#     assert tax_from_saving_interest(0, 20000) == 286
-
-# def test_tax_from_saving_interest_scenario_3(): 
-#     '''
-#     Hannah: Earns £35,000 from work, has £5,000 of savings income
-
-#     Hannah will pay tax of £800 on her savings interest.
-#     The first £12,570 of her income from work is tax free, but she'll pay basic-rate 20% tax on the amount above that. 
-#     As she earns more than £17,570, there's no starting rate for savings available to her.
-#     However, as a basic-rate taxpayer, she does get a personal savings allowance of £1,000, which covers some of her interest. 
-#     However, the remaining interest of £4,000 will be taxed at 20%, meaning she'll pay £800 to HMRC, on top of the tax she pays on her salary 
-#     (though, as we're MSE, we'd suggest she shelters some of her savings in an ISA, legally cutting her tax bill). 
-#     '''
-#     assert tax_from_saving_interest(35000, 5000) == 800
+    Hannah will pay tax of £800 on her savings interest.
+    The first £12,570 of her income from work is tax free, but she'll pay basic-rate 20% tax on the amount above that. 
+    As she earns more than £17,570, there's no starting rate for savings available to her.
+    However, as a basic-rate taxpayer, she does get a personal savings allowance of £1,000, which covers some of her interest. 
+    However, the remaining interest of £4,000 will be taxed at 20%, meaning she'll pay £800 to HMRC, on top of the tax she pays on her salary 
+    (though, as we're MSE, we'd suggest she shelters some of her savings in an ISA, legally cutting her tax bill). 
+    '''
+    assert tax_from_saving_interest(35000, 5000) == 800
 
 
 # get more scenario testing from this: 
-# https://blog.moneysavingexpert.com/martins-blog/2016/02/the-new-personal-savings-allowance-means-some-people-will-be-better-off-earning-less-interest/
+# https://blog.moneysavingexpert.com/2016/02/the-new-personal-savings-allowance-means-some-people-will-be-better-off-earning-less-interest/
+# modified to suit the 2025 tax bucket
+def test_tax_from_saving_interest_scenario_4(): 
+    '''
+    Near the limit but not over it
+
+    Income from work: £49270
+    Interest from savings accounts: £990
+    Total income: £50,260 - so less than the higher-rate threshold
+    Size of PSA: £1,000
+    Total amount of interest earned after tax: £990 as all your interest is covered by the PSA
+
+    In this scenario all of your savings interest would be tax-free.
+    '''
+    assert calculate_income_tax(49270, 990) == 7340
+    assert calculate_income_tax(49270) == 7340
+    assert tax_from_saving_interest(49270, 990) == 0
+
+def test_tax_from_saving_interest_scenario_5(): 
+    '''
+    You earn £20 more savings interest, but take home LESS
+
+    Income from work: £49,270
+    Interest from savings accounts: £1,010
+    Total income: £50,280 - so you are a higher-rate taxpayer
+    Size of PSA: £500
+    Total amount of interest earned after tax: £906 (£500 tax-free, £500 taxed at 20% and £10 taxed at 40%)
+
+    Therefore, bizarrely, the fact you've earned slightly more interest means because a chunk more of it is taxed you actually end up taking home less. 
+    You would actually have been slightly better off to have earned £10 less interest.
+
+    A similar situation will occur for those at the brink of hitting the £150,000 earnings barrier where you become a top 45% rate taxpayer.
+    In this scenario all of your savings interest would be tax-free.
+    '''
+    assert calculate_income_tax(49270, 1010) == 7444 # 7340 + 104
+    assert tax_from_saving_interest(49270, 1010) == 104 # 500 tax-free, 500 taxed at 20%, 10 taxed at 40%
+    # this didn't pass, which means the tax_from_saving_interest/ function needs to be fixed
